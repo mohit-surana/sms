83a7a633634211f373a39be36a076ed41dd96717
student <mohit.surana95@gmail.com>
2016-09-27 09:03:21 +0530

    Lab 8 - Autocorrelation Test

--- lab8/autocorrelation.py
+++ lab8/autocorrelation.py
@@ -25,7 +25,14 @@ sigma_rho_im_cap = sqrt(13*M + 7) / 12(M+1)
 Z = rho_im_cap / sigma_rho_im_cap
 '''
 
+# To debug, use N=30, i=1, m= 4 and the below list
+# R = [0.32, 0.1, 0.23, 0.28, 0.89, 0.31, 0.64, 0.28, 0.83, 0.93, 0.99, 0.15, 0.33, 0.35, 0.91, 0.41, 0.6, 0.27, 0.75, 0.88, 0.68, 0.49, 0.5, 0.43, 0.95, 0.58, 0.19, 0.36, 0.69, 0.87]
+
 R = random_numbers
+
+# This is done because we interpret i=1 as the first number
+i -= 1
+
 total = 0
 for k in range(M+1):
 	total += R[i+k*m] * R[i+(k+1)*m]
@@ -36,6 +43,7 @@ Z = rho_im_cap / sigma_rho_im_cap
 
 alpha = 0.05
 Z_table = { alpha/2: 1.96 }
+
 print('The value of Z is', Z)
 print('The value of Z_table is', Z_table[alpha/2])
 if(-Z_table[alpha/2] <= Z <= Z_table[alpha/2]):
